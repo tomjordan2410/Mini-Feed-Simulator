5835fcf1ed6b7e387c2308aafe160a12f4b75611
tomjordan2410 <tomjordan2410@gmail.com>
2026-09-12 07:31:53 +0100

    Semicolons added

--- mini-feed.js
+++ mini-feed.js
@@ -1,4 +1,4 @@
-let feed = []
+let feed = [];
 
 function addPost(username, content, timestamp = Date.now()) {
     let newPost = {
@@ -13,47 +13,46 @@ function addPost(username, content, timestamp = Date.now()) {
 }
 
 function likePost(postId) {
-    let post = feed.find(post => post.id === postId)
+    let post = feed.find(post => post.id === postId);
     if (post) {
-        post.likes++
+        post.likes++;
     }
     if (!post) {
-        console.log("No post found")
-        return
+        console.log("No post found");
+        return;
     }
 }
 
 function getFeed() {
-    return feed 
+    return feed;
 }
 
 function sortByNewest() {
-    return feed.toSorted((a, b) => b.timestamp - a.timestamp)
+    return feed.toSorted((a, b) => b.timestamp - a.timestamp);
 }
 
 function sortByLikes() {
-    return feed.toSorted((a, b) => b.likes - a.likes)
+    return feed.toSorted((a, b) => b.likes - a.likes);
 }
 
 function getPostsByUser(username) {
-    let filteredPosts = feed.filter(post => post.username === username)
+    let filteredPosts = feed.filter(post => post.username === username);
     if (filteredPosts.length === 0) {
-        console.log("User not Found")
-        return
+        console.log("User not Found");
+        return;
     }
-    return filteredPosts
+    return filteredPosts;
 }
 
 // ************************************
 
-const now = Date.now()
+const now = Date.now();
 
-addPost("Tom", "Here's my first post", now - 10000)
-addPost("Steve", "Here's the second post", now - 5000)
-addPost("Brent", "here's the third post", now)
-likePost(1)
-likePost(1)
-likePost(2)
-likePost(2)
-likePost(2)
-console.log(getPostsByUser("Collin"))
+addPost("Tom", "Here's my first post", now - 10000);
+addPost("Steve", "Here's the second post", now - 5000);
+addPost("Brent", "here's the third post", now);
+likePost(1);
+likePost(1);
+likePost(2);
+likePost(2);
+likePost(2);
